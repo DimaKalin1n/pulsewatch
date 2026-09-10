1cd36ff3dcab2d86c7c23355f0184f3a3058ebd7
DimaKalin1n <dimakalinin1207@gmail.com>
2026-09-10 21:33:42 +0300

    Составил model

--- src/main/java/monitors/model/Monitor.java
+++ src/main/java/monitors/model/Monitor.java
@@ -1,5 +1,20 @@
 package monitors.model;
 
+import jakarta.persistence.*;
+
 @Entity
+@Table(name="monitors")
 public class Monitor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String url;
+
+    private int intervalSeconds;
+
+    private boolean enable;
 }
